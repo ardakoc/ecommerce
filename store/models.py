8332943168c2bbc46db040308883decc75154703
ardakoc <ardaskoc@gmail.com>
2022-06-08 15:42:26 +0300

    Adding image field to the product model

--- store/models.py
+++ store/models.py
@@ -25,7 +25,7 @@ class Order(models.Model):
   date_ordered   = models.DateTimeField(auto_now_add=True)
   complete       = models.BooleanField(default=False)
   transaction_id = models.CharField(max_length=100, null=True)
-  #image         = ...
+  image          = models.ImageField(null=True, blank=True)
 
   def __str__(self):
     return str(self.id)
