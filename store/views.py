be235ac8529622f62b9cc98fdd36ee12f618586b
ardakoc <ardaskoc@gmail.com>
2022-06-18 17:36:25 +0300

    Rendering the cart data

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from store.models import Product
+from store.models import Order, Product
 
 # Create your views here.
 
@@ -11,7 +11,14 @@ def store(request):
 
 
 def cart(request):
-  context = {}
+  if request.user.is_authenticated:
+    customer = request.user.customer
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    items = order.orderitem_set.all()
+  else:
+    items = []
+
+  context = {'items': items}
   return render(request, 'store/cart.html', context)
 
 
