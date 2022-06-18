ef18129e82d5ffb4001cdfaafc0d30112b65a4ab
ardakoc <ardaskoc@gmail.com>
2022-06-18 18:01:15 +0300

    Rendering the cart totals

--- store/views.py
+++ store/views.py
@@ -16,12 +16,23 @@ def cart(request):
     order, created = Order.objects.get_or_create(customer=customer, complete=False)
     items = order.orderitem_set.all()
   else:
+    # create empty cart for now for unauthenticated users
+    order = {'get_cart_total': 0, 'get_cart_items': 0}
     items = []
 
-  context = {'items': items}
+  context = {'order': order, 'items': items}
   return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-  context = {}
+  if request.user.is_authenticated:
+    customer = request.user.customer
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    items = order.orderitem_set.all()
+  else:
+    # create empty cart for now for unauthenticated users
+    order = {'get_cart_total': 0, 'get_cart_items': 0}
+    items = []
+
+  context = {'order': order, 'items': items}
   return render(request, 'store/checkout.html', context)
