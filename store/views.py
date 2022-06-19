d6f9c92ad5fa20da694d54d5f2eae6d5cbb7d604
ardakoc <ardaskoc@gmail.com>
2022-06-19 11:45:07 +0300

    Adding update item methods and add to cart functionality

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
 
-from store.models import Order, Product
+from store.models import Order, OrderItem, Product
 
 # Create your views here.
 
@@ -36,3 +38,28 @@ def checkout(request):
 
   context = {'order': order, 'items': items}
   return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+  data = json.loads(request.body)
+  productId = data['productId']
+  action = data['action']
+  print('action:', action)
+  print('product:', productId)
+
+  customer = request.user.customer
+  product = Product.objects.get(id=productId)
+  order, created = Order.objects.get_or_create(customer=customer, complete=False)
+  orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+  
+  if action == 'add':
+    orderItem.quantity = (orderItem.quantity+1)
+  elif action == 'remove':
+    orderItem.quantity = (orderItem.quantity-1)
+
+  orderItem.save()
+
+  if orderItem.quantity <= 0:
+    orderItem.delete()
+
+  return JsonResponse('Item was added', safe=False)
